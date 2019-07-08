7c2c8dcdfc4a8859cc0c9d5b1cdf62190fc49c9a
Kim Thompson <kthompson@mpr.org>
2019-07-08 15:00:32 -0500

    got it working

--- src/Slideshow/Slidecredit.js
+++ src/Slideshow/Slidecredit.js
@@ -4,15 +4,17 @@ import Link from 'next/link';
 
 const SlideCredit = (props) => {
   const { creditName, creditUrl } = props;
-  {
-    if (creditName && creditUrl)
-      return (
-        <Link href={creditUrl}>
-          <a className="slideshow_creditlink">{creditName}</a>
-        </Link>
-      );
+  if (creditName && creditUrl) {
+    return (
+      <Link href={creditUrl}>
+        <a className="slideshow_creditlink">{creditName}</a>
+      </Link>
+    );
+  } else if (creditName) {
+    return <div>{creditName}</div>;
+  } else {
+    return null;
   }
-  if (creditName) return <div>{creditName}</div>;
 };
 
 SlideCredit.propTypes = {
