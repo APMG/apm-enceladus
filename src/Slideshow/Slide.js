52bd264065ad4a288a8f2728ffdbc25dd872938e
Andrea Edstrom <aedstrom@mpr.org>
2019-07-14 16:14:47 -0500

    update version to 0.3.0-beta.0

--- src/Slideshow/Slide.js
+++ src/Slideshow/Slide.js
@@ -1,42 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Image } from 'apm-mimas';
 import SlideCredit from './SlideCredit';
 
-class Slide extends Component {
-  render() {
-    return (
+const Slide = (props) => {
+  return (
+    <>
+      <div className="slideshow_count">
+        {props.image.index + 1} of {props.max}
+      </div>
       <figure className="slideshow_figure">
         <Image
-          image={this.props.image}
+          image={props.image}
           aspectRatio="widescreen"
           elementClass="slideshow_image"
         />
+
         <figcaption className="slideshow_caption">
-          {this.props.image.credit && (
+          {props.image.credit && (
             <div className="slideshow_credit">
               <SlideCredit
-                name={this.props.image.credit}
-                url={this.props.image.credit_url}
+                name={props.image.credit}
+                url={props.image.credit_url}
               />
             </div>
           )}
-          {this.props.image.long_caption}
-
-          <br />
-          <em>
-            ({this.props.image.index + 1} of {this.props.max})
-          </em>
+          {props.image.long_caption}
         </figcaption>
       </figure>
-    );
-  }
-}
+    </>
+  );
+};
 
 Slide.propTypes = {
   image: PropTypes.object,
   max: PropTypes.number.isRequired,
-  credit: PropTypes.any
+  prev: PropTypes.func,
+  next: PropTypes.func
 };
 
 export default Slide;
