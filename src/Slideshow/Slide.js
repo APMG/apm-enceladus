bceb54ec9fa04156f881db6c6f77ae3267bc4e52
Andrea Edstrom <aedstrom@mpr.org>
2019-08-27 11:09:13 -0500

    respect crop of photos and handle height in fullscreen state

--- src/Slideshow/Slide.js
+++ src/Slideshow/Slide.js
@@ -1,42 +1,98 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Image } from '@apmg/mimas';
 import SlideCredit from './SlideCredit';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
 
-const Slide = (props) => {
-  return (
-    <>
-      <div className="slideshow_count">
-        {props.image.index + 1} of {props.max}
+class Slide extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      size: 0,
+      slideWidth: '100%'
+    };
+  }
+
+  getFigureHeight = (size) => {
+    this.setState({ size: size });
+  };
+
+  refSlideCallback = (element) => {
+    if (element) {
+      var intFrameHeight = window.innerHeight;
+      var captionHeight;
+
+      if (element.offsetHeight >= 100) {
+        //mobile will be taller than 100
+        captionHeight = element.offsetHeight;
+      } else {
+        //desktop will usually be less than 100
+        // keep height consistent between long captions and short captions
+        captionHeight = 100;
+      }
+
+      if (captionHeight === undefined || 0) {
+        return;
+      }
+      let imgHeight = intFrameHeight - 100;
+      this.getFigureHeight(imgHeight);
+    }
+  };
+
+  render() {
+    const { size } = this.state;
+    let dynamicStyle = () => css`
+      > img {
+        max-height: ${size}px;
+        width: auto;
+      }
+    `;
+    let ManageFullScreenHeight = styled('div')`
+      ${dynamicStyle}
+    `;
+
+    return (
+      <div className="slideshow_slide">
+        <div className="slideshow_count">
+          {this.props.image.index + 1} of {this.props.max}
+        </div>
+        <figure className="slideshow_figure">
+          <ManageFullScreenHeight>
+            <Image
+              image={this.props.image}
+              aspectRatio={
+                this.props.image.aspect_ratios.preferred_aspect_ratio_slug
+              }
+              elementClass="slideshow_image"
+            />
+          </ManageFullScreenHeight>
+
+          <figcaption className="slideshow_caption" ref={this.refSlideCallback}>
+            {this.props.image.credit && (
+              <div className="slideshow_credit">
+                <SlideCredit
+                  name={this.props.image.credit}
+                  url={this.props.image.credit_url}
+                />
+              </div>
+            )}
+            {this.props.image.long_caption}
+          </figcaption>
+        </figure>
       </div>
-      <figure className="slideshow_figure">
-        <Image
-          image={props.image}
-          aspectRatio="widescreen"
-          elementClass="slideshow_image"
-        />
-
-        <figcaption className="slideshow_caption">
-          {props.image.credit && (
-            <div className="slideshow_credit">
-              <SlideCredit
-                name={props.image.credit}
-                url={props.image.credit_url}
-              />
-            </div>
-          )}
-          {props.image.long_caption}
-        </figcaption>
-      </figure>
-    </>
-  );
-};
+    );
+  }
+}
 
 Slide.propTypes = {
   image: PropTypes.object,
   max: PropTypes.number.isRequired,
   prev: PropTypes.func,
-  next: PropTypes.func
+  next: PropTypes.func,
+  refSlideCallback: PropTypes.func,
+  getSlideshowSize: PropTypes.func,
+  size: PropTypes.number
 };
 
 export default Slide;
--- src/Slideshow/Slideshow.js
+++ src/Slideshow/Slideshow.js
@@ -1,339 +1,36 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import IconChevronRight from './svg/IconChevronRight';
-import IconChevronLeft from './svg/IconChevronLeft';
-import IconFullscreen from './svg/IconFullscreen';
-import IconClose from './svg/IconClose';
-import Poses from './Poses';
-import { animationDuration } from './animations';
 import FocusTrap from 'focus-trap-react';
+import PropTypes from 'prop-types';
+import SlideshowInner from './SlideshowInner';
 
 // Links will not work with react-swipeable. If you need to add links, we might have to consider removing the swiping capabilities. Should I try react-swipeable-views? react-easy-swipe?
 
 class Slideshow extends Component {
   constructor(props) {
     super(props);
-    this.fullscreenRef = React.createRef();
-    this.slideshowRef = React.createRef();
-    this.slideshowBgRef = React.createRef();
-    let images = props.images;
-
-    // Adds an index property to each image
-    images.forEach((image, i) => {
-      image.index = i;
-    });
 
     this.state = {
-      index: 0,
-      images: images,
-      disabled: false,
       isFullscreen: false,
       activeTrap: false
     };
   }
 
-  prevIndex(i) {
-    return i > 0 ? i - 1 : this.state.images.length - 1;
-  }
-
-  nextIndex(i) {
-    return i < this.state.images.length - 1 ? i + 1 : 0;
-  }
-
-  getNearestImages = (images, index) => {
-    if (images.length < 3) {
-      return images;
-    } else {
-      return [
-        images[this.prevIndex(index)],
-        images[index],
-        images[this.nextIndex(index)]
-      ];
-    }
-  };
-
-  prev = () => {
-    this.setState({
-      index: this.prevIndex(this.state.index),
-      disabled: true
-    });
-
-    setTimeout(() => {
-      this.setState({ disabled: false });
-    }, animationDuration);
-  };
-
-  next = () => {
-    this.setState({
-      index: this.nextIndex(this.state.index),
-      disabled: true
-    });
-
-    setTimeout(() => {
-      this.setState({ disabled: false });
-    }, animationDuration);
-  };
-
-  fullscreen = () => {
-    const body = document.body;
-    if (!this.state.isFullscreen) {
-      if (this.fullscreenRef.current) {
-        this.fullscreenRef.current.focus();
-      }
-      body.style.height = '100vh';
-      body.style.overflowY = 'hidden';
-    }
-    if (this.state.isFullscreen) {
-      body.style.height = '100%';
-      body.style.overflowY = 'visible';
-    }
-    this.setState({
-      isFullscreen: !this.state.isFullscreen,
-      activeTrap: !this.state.activeTrap
-    });
-  };
-
-  isImageOnclickActive = () => {
-    //if the state is fullscreen disable onclick
-    if (!this.state.isFullscreen) {
-      this.fullscreen();
-    }
-    //if the state is fullscreen disable onclick
-    if (this.state.isFullscreen) {
-      return;
-    }
-  };
-
-  isBgOnclickActive = () => {
-    //if the state is fullscreen disable onclick
-    if (this.state.isFullscreen) {
-      this.fullscreen();
-    }
-    //if the state is fullscreen disable onclick
-    if (!this.state.isFullscreen) {
-      return;
-    }
-  };
-
-  wrapKeyHandler = (event) => {
-    if (event.keyCode === 27 && this.state.isFullscreen) {
-      // escape key
-      this.fullscreen();
-      this.fullscreenRef.current.focus();
-    }
-  };
-
   render() {
-    const classes = classNames({
-      slideshow: true,
-      [this.props.elementClass]: this.props.elementClass
-    });
     const { activeTrap } = this.state;
-
     return (
       <>
         {activeTrap ? (
           <FocusTrap>
-            <div
-              id="slideshow"
-              data-testid="slideshow"
-              className={`${
-                this.state.isFullscreen ? classes + ' fullscreen' : classes
-              }`}
-              ref={this.slideshowRef}
-            >
-              <button
-                aria-haspopup="true"
-                aria-label="fullscreen slideshow"
-                data-testid="fullscreen-button"
-                className="slideshow_fullscreen"
-                onClick={this.fullscreen}
-                ref={this.fullscreenRef}
-                onKeyUp={this.wrapKeyHandler}
-              >
-                {!this.state.isFullscreen && (
-                  <>
-                    <IconFullscreen elementClass="slideshow_icon slideshow_icon-fullscreen" />
-                    <span className="invisible" data-testid="icon-fullscreen">
-                      Fullscreen Slide
-                    </span>
-                  </>
-                )}
-                {this.state.isFullscreen && (
-                  <>
-                    <IconClose
-                      elementClass="slideshow_icon slideshow_icon-shrink"
-                      aria-label="close"
-                    />
-                    <span className="invisible" data-testid="icon-shrink">
-                      Shrink Slide
-                    </span>
-                  </>
-                )}
-              </button>
-              <button
-                data-testid="prev-button"
-                aria-label="Icon Chevron Left"
-                className="slideshow_button slideshow_button-prev"
-                onClick={this.prev}
-              >
-                <IconChevronLeft elementClass="slideshow_icon" />
-                <span className="invisible">Previous Slide</span>
-              </button>
-              <>
-                {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
-                <div
-                  className={`${
-                    this.state.isFullscreen
-                      ? 'slideshow_container fullscreen'
-                      : 'slideshow_container'
-                  }`}
-                  aria-modal={this.state.isFullscreen}
-                  aria-haspopup="true"
-                  role="dialog"
-                  onClick={this.isImageOnclickActive}
-                  onKeyUp={this.wrapKeyHandler}
-                >
-                  {this.getNearestImages(
-                    this.state.images,
-                    this.state.index
-                  ).map((image) => (
-                    <Poses
-                      key={image.index}
-                      animation={this.props.animation}
-                      image={image}
-                      stateIndex={this.state.index}
-                      max={this.state.images.length}
-                    />
-                  ))}
-                </div>
-              </>
-
-              <button
-                data-testid="next-button"
-                aria-label="Icon Chevron Right"
-                className="slideshow_button slideshow_button-next"
-                onClick={this.next}
-              >
-                <IconChevronRight elementClass="slideshow_icon" />
-                <span className="invisible">Next Slide</span>
-              </button>
-              {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
-              <div
-                id="slideshowBg"
-                onClick={this.isBgOnclickActive}
-                onKeyUp={this.wrapKeyHandler}
-                role="figure"
-                data-testid="slideshowBg"
-                className={`${
-                  this.state.isFullscreen
-                    ? 'slideshow_bg fullscreen'
-                    : 'slideshow_bg'
-                }`}
-                ref={this.slideshowBgRef}
-              />
-            </div>
+            <SlideshowInner
+              isFullscreen={this.state.isFullscreen}
+              images={this.props.images}
+            />
           </FocusTrap>
         ) : (
-          <div
-            id="slideshow"
-            data-testid="slideshow"
-            className={`${
-              this.state.isFullscreen ? classes + ' fullscreen' : classes
-            }`}
-            ref={this.slideshowRef}
-          >
-            <button
-              aria-haspopup="true"
-              aria-label="fullscreen slideshow"
-              data-testid="fullscreen-button"
-              className="slideshow_fullscreen"
-              onClick={this.fullscreen}
-              ref={this.fullscreenRef}
-              onKeyUp={this.wrapKeyHandler}
-            >
-              {!this.state.isFullscreen && (
-                <>
-                  <IconFullscreen elementClass="slideshow_icon slideshow_icon-fullscreen" />
-                  <span className="invisible" data-testid="icon-fullscreen">
-                    Fullscreen Slide
-                  </span>
-                </>
-              )}
-              {this.state.isFullscreen && (
-                <>
-                  <IconClose
-                    elementClass="slideshow_icon slideshow_icon-shrink"
-                    aria-label="close"
-                  />
-                  <span className="invisible" data-testid="icon-shrink">
-                    Shrink Slide
-                  </span>
-                </>
-              )}
-            </button>
-            <button
-              data-testid="prev-button"
-              aria-label="Icon Chevron Left"
-              className="slideshow_button slideshow_button-prev"
-              onClick={this.prev}
-            >
-              <IconChevronLeft elementClass="slideshow_icon" />
-              <span className="invisible">Previous Slide</span>
-            </button>
-            <>
-              {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
-              <div
-                className={`${
-                  this.state.isFullscreen
-                    ? 'slideshow_container fullscreen'
-                    : 'slideshow_container'
-                }`}
-                aria-modal={this.state.isFullscreen}
-                aria-haspopup="true"
-                role="dialog"
-                onClick={this.isImageOnclickActive}
-                onKeyUp={this.wrapKeyHandler}
-              >
-                {this.getNearestImages(this.state.images, this.state.index).map(
-                  (image) => (
-                    <Poses
-                      key={image.index}
-                      animation={this.props.animation}
-                      image={image}
-                      stateIndex={this.state.index}
-                      max={this.state.images.length}
-                    />
-                  )
-                )}
-              </div>
-            </>
-
-            <button
-              data-testid="next-button"
-              aria-label="Icon Chevron Right"
-              className="slideshow_button slideshow_button-next"
-              onClick={this.next}
-            >
-              <IconChevronRight elementClass="slideshow_icon" />
-              <span className="invisible">Next Slide</span>
-            </button>
-            {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
-            <div
-              id="slideshowBg"
-              onClick={this.isBgOnclickActive}
-              onKeyUp={this.wrapKeyHandler}
-              role="figure"
-              data-testid="slideshowBg"
-              className={`${
-                this.state.isFullscreen
-                  ? 'slideshow_bg fullscreen'
-                  : 'slideshow_bg'
-              }`}
-              ref={this.slideshowBgRef}
-            />
-          </div>
+          <SlideshowInner
+            isFullscreen={this.state.isFullscreen}
+            images={this.props.images}
+          />
         )}
       </>
     );
@@ -417,14 +114,6 @@ Slideshow.propTypes = {
       url: PropTypes.string,
       srcset: PropTypes.string
     })
-  ).isRequired,
-  animation: PropTypes.oneOf(['fade', 'slide']),
-  elementClass: PropTypes.string
+  ).isRequired
 };
-// The animation object is allowed to be "loose". It's just potentially too variable to test here, and if you mess it up, it shouldn't crash anything -- your animation just won't work.
-
-Slideshow.defaultProps = {
-  animation: 'fade'
-};
-
 export default Slideshow;
